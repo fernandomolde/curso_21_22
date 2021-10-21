9d6938baa424a773abb14685f6707be608446a43
fernandomolde <ortizmoldef@gmail.com>
2021-10-21 10:33:11 +0200

    Funcion primer dia

---
+++ funciones_1.py
@@ -0,0 +1,25 @@
+# def primera(mensaje, usuario,tema):
+#     #return mensaje + usuario + tema
+#         pass
+
+# v1 = primera('Hola mundo', 'fer', 'clase de funciones')
+# print(v1)
+
+# def segunda():
+#     v1 = 'Hola'
+#     v2 = 'caracola'
+    
+#     return v1,v2
+
+# a1,a2 = segunda()
+# print(a1)
+# print(a2)
+
+
+# def tercera(uno=1):
+#     return uno
+
+# print(tercera())
+
+# funcion larga(*args, **kwargs)
+# funcion larga(1,2,3,4)
