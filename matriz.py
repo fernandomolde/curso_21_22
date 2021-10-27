07c830f8cefaf1abb1709abed0c36628b3a434e3
fernandomolde <ortizmoldef@gmail.com>
2021-10-27 09:06:37 +0200

    'Matrices'

---
+++ matriz.py
@@ -0,0 +1,9 @@
+lista = [1,2,3,4,'A']
+lista2 = [5,6,7,8,'B']
+lista3 = [9,10,11,12,'C']
+lista4 = [13,14,15,16,'D']
+         
+
+for i in range(5):
+    x = [lista,lista2,lista3,lista4] 
+    print(x)
