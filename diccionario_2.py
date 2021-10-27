bd972e4f6f982033f0ecd0ce038f386e0b3557b4
fernandomolde <ortizmoldef@gmail.com>
2021-10-27 11:01:45 +0200

    diccionario

---
+++ diccionario_2.py
@@ -0,0 +1,9 @@
+from pprint import pprint
+
+variable = [('campo1','valor1'),
+            ('campo2','valor2'),                #Crea un diccionario
+            ('campo3','valor3')]
+
+
+dic = dict(variable)
+pprint(dic)
