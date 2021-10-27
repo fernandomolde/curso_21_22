83d9afbe6ec43816ee4d281f4b47723dc667d9e7
fernandomolde <ortizmoldef@gmail.com>
2021-10-27 11:25:36 +0200

    diccionario

--- diccionario_2.py
+++ diccionario_2.py
@@ -1,9 +1,27 @@
-from pprint import pprint
+from pprint import pp, pprint
 
 variable = [('campo1','valor1'),
             ('campo2','valor2'),                #Crea un diccionario
-            ('campo3','valor3')]
+            ('campo3','valor3'),
+            ('campo4','valor4'),
+            ('campo5','valor5'),                #Crea un diccionario
+            ('campo6','valor6')]
 
 
 dic = dict(variable)
-pprint(dic)
+pprint(dic.items())
+
+# for k in dic:
+#     print(dic[k])
+
+for k,v in dic.items():
+    print(k,v)
+
+claves = dic.keys()
+valores = dic.values()
+pprint(claves)
+pprint(valores)
+
+for i in claves:
+    print(i)
+    
