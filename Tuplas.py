5dc865368b3bd7ecdcd0f8567142be2e3093e33c
fernandomolde <ortizmoldef@gmail.com>
2021-10-27 10:12:22 +0200

    'Tupla'

---
+++ Tuplas.py
@@ -0,0 +1,18 @@
+t = (1,)  # la coma hace que diga que es una tupla
+print(type(t))     
+
+# w = list(t)
+# w[2] = 0 esto es para cambiar la tupla a lista Y cambiarla
+# print(w)
+# # print(t[2])
+# print(t)
+# t1 = (0,9,8)
+# t2 = t + t1
+
+# print(t2)
+
+# for elem in t:
+#     print(elem)     Para recorrer la lista
+
+# for i in range (len(t)):
+#     print(t[i])
