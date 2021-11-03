033918fc3e123b8aca5845de0175da6496297f65
fernandomolde <ortizmoldef@gmail.com>
2021-11-03 08:53:47 +0100

    Actividad diccionario

---
+++ fun_datos.py
@@ -0,0 +1,13 @@
+def introduce():
+    personas = {}
+
+for i in range(1):
+    nombre = input('Introduzca nombre: ')
+    apellidos = input('Introduzca apellidos')
+    dni = input('Introduzca DNI: ')
+    personas[dni] = {'nombre',nombre, 'apellidos':apellidos}
+
+    return personas 
+
+introduce()
+print(introduce())
