bd0beb77ce0934561bb0fc72e4e1f7cbd46b3b35
fernandomolde <ortizmoldef@gmail.com>
2021-11-05 11:11:08 +0100

    Rutas

---
+++ rutas_actividad.py
@@ -0,0 +1,35 @@
+import os
+import settings
+
+
+os.system('clear')
+
+lista_archivo = []
+archivo_salida = 'salida.txt'
+ruta_salida = settings.RUTA_BASE + settings.CODIGO + settings.MI_CARPETA
+miembros = 5
+fila = ''
+# Buscar los archivos
+archivo = os.scandir(settings.RUTA_BASE + settings.CODIGO)
+for a in archivo:
+    if a.name.endswith('.py'):
+        lista_archivo.append(a.name[:-3:])
+
+
+# Agrupalos de 5 en 5 
+for i in range(0,len(lista_archivo),miembros):
+    temp = lista_archivo[i: i + miembros]
+    fila += ','.join(temp) + '\n'
+
+
+
+# Guardar archivos en el fichero 
+nuevo = open(ruta_salida + '/lista_py.txt','w')    
+nuevo.write(fila)
+nuevo.close()
+
+
+
+
+
+
---
+++ settings.py
@@ -0,0 +1,3 @@
+RUTA_BASE = '/home/fer/codigo'
+CODIGO = '/curso_21_22'
+MI_CARPETA = '/viernes/programacion/python'
