d76330a7509d991e8fcf0f39991d6082d81e1cc0
fernandomolde <ortizmoldef@gmail.com>
2021-11-10 11:27:27 +0100

    Ficheros y Rutas

--- fichero_a_dict.py
+++ fichero_a_dict.py
@@ -7,16 +7,29 @@ ruta = '/home/fer/codigo/curso_21_22/viernes/programacion/python/funciones_mierc
 
 dic_salida = {}
 
-clave = 0
+
+
+def modo1():
+
+    clave = 0
 
 #Leer Archivo
-with open(ruta) as archivo:
-    for l in archivo:
-        # Procesar fila a fila
-        fila = l[:-1:].split(',')
-        #Recorrer lista y llenar dict
-        for nombre in fila:
-            dic_salida[clave] = nombre
-            clave +=1
+    with open(ruta) as archivo:
+        for l in archivo:
+            # Procesar fila a fila
+            fila = l[:-1:].split(',')
+            #Recorrer lista y llenar dict
+            for nombre in fila:
+                dic_salida[clave] = nombre
+                clave +=1
 
-pprint.pprint(dic_salida)
+
+def modo2():
+    clave = 0
+    texto = open(ruta,'r').readlines()
+    for archivo in texto:
+        dic_salida[clave] = archivo
+        clave +=1
+    return dic_salida
+
+pprint.pprint(modo2())  
