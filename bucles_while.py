78ac84bbd9a0ed167ebb245119ca598f7b44467c
fernandomolde <ortizmoldef@gmail.com>
2021-11-22 09:09:08 +0100

    titanic

---
+++ bucles_while.py
@@ -0,0 +1,12 @@
+
+respuesta = ''
+while respuesta != 'n':
+    respuesta = input('¿Terminar?')
+    break
+
+for i in range(100):
+    if i == 10:
+        break
+    print(i)
+    if i == 15:
+        break
