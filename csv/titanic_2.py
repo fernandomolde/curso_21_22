e8eddadebc063488713a348b9d56f3f2bb0208fd
fernandomolde <ortizmoldef@gmail.com>
2021-11-22 10:32:02 +0100

    Titanic

--- csv/titanic_2.py
+++ csv/titanic_2.py
@@ -1,6 +1,6 @@
 import os
 import csv
-import pprint   
+import pprint
 
 os.system('clear')
 
@@ -15,29 +15,59 @@ def leer_archivo():
     return lista_dict
 
 
-def procesar_csv():
-    pasajeros = leer_archivo()
-    mujeres = []
-    hombres = []
-    h_v = 0
-    h_m = 0
-    m_v = 0
-    m_m = 0
-    for p in pasajeros:
-         if p['Sex'] == 'female':
-            mujeres.append(p['Survived'])
-    else:                                                       #Paso 2 y 3 
-            hombres.append(p['Survived'])
-    h_v = hombres.count('1')
-    h_m = mujeres.count('0')
-    m_v = mujeres.count('1')
-    m_m = mujeres.count('0')
+# def procesar_csv():
+#     pasajeros = leer_archivo()
+#     mujeres = []
+#     hombres = []
+#     h_v = 0
+#     h_m = 0
+#     m_v = 0
+#     m_m = 0
+#     for p in pasajeros:
+#          if p['Sex'] == 'female':
+#             mujeres.append(p['Survived'])
+#     else:                                                       #Paso 2 y 3 
+#             hombres.append(p['Survived'])
+#     h_v = hombres.count('1')
+#     h_m = mujeres.count('0')
+#     m_v = mujeres.count('1')
+#     m_m = mujeres.count('0')
     
-    return (h_m, h_v, m_m, m_v)
+#     return (h_m, h_v, m_m, m_v)
 
 # 4- Devuelve resultado
 
-resultado = procesar_csv()
-print(f'Hombre vivos : {resultado[1]} Hombres Muertos : {resultado[0]}' )
-print(f'Mujeres vivas : {resultado[3]} Mujeres Muertas : {resultado[2]}' )
+# resultado = procesar_csv()
+# print(f'Hombre vivos : {resultado[1]} Hombres Muertos : {resultado[0]}' )
+# print(f'Mujeres vivas : {resultado[3]} Mujeres Muertas : {resultado[2]}' )
+
+
+lista_diccionario = leer_archivo()
+def personas_20_30():
+    hay_cabecera = False
+    for i in lista_diccionario:
+        if i['Age'] >= '20' and i ['Age'] <= '30' and i ['Survived'] == '1':
+            with open (ruta + 'titanic_nombres.csv', 'a') as csv_writer:
+                escribir = csv.writer(csv_writer)
+                if not hay_cabecera:
+                    escribir.writerow(["Nombre", "Clase", "Sexo"])
+                    hay_cabecera = True
+                else:
+                    escribir.writerow([i["Name"],i["Pclass"],i["Sex"]])
+
+
+personas_20_30()
+
+
+
+    
+
+
+
+
+
+
+
+
+
 
