1b71f36c0a18474c45531ecc90bd95ad87169e53
fernandomolde <ortizmoldef@gmail.com>
2021-12-02 10:05:32 +0100

    OOP

--- OOP/10_polimorfismo_herencia.py
+++ OOP/10_polimorfismo_herencia.py
@@ -1,17 +1,25 @@
 class Pajaro():
-    def cantar(self):
+     def __str__(self) -> str:
+        return 'Pajaro'
+     
+     def cantar(self):
         print('Los pájaros tienen cantos diferentes')
 
 class Gorrion(Pajaro):
+    def __str__(self) -> str:
+        return 'Gorrion'
+
     def cantar(self, con_padre = False):
         if con_padre:
             super().cantar()
         print('Gorrión pio,pio')
     
 class Gallo(Pajaro):
-    def cantar(self, con_padre = False):
+      def __str__(self) -> str:
+        return 'Gallo'
+      def cantar(self, con_padre = False):
         if con_padre:
-            super().cantar()
+         super().cantar()
         print('Gallo kikirikiii')
     
 
@@ -28,9 +36,12 @@ class CoroPajaros():
     def cantar(self):
         titulo = True
         for p in self.coro:
-            p.cantar(titulo)
-            titulo = False
-
+            # p.cantar(titulo)
+            # titulo = False
+            if issubclass(p.__class__,Pajaro):
+                print(f'Este es un pajaro : {p}')
+            else:
+                print('Este NO es un pajaro: {p}')
 lista_pajaros = ['p',1,'l']     #[Gallo(),Gallo(),Gorrion()]
 c = CoroPajaros(lista_pajaros)
 c.cantar()
