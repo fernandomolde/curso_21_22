2ce57d6b11c33a7178f1212de9b3e7cc9c4f7ac2
fernandomolde <ortizmoldef@gmail.com>
2022-01-10 10:32:23 +0100

    OPP

--- OOP/13-decoradores.py
+++ OOP/13-decoradores.py
@@ -1,26 +1,44 @@
+import os
+os.system('clear')
+
 """
 Son funciones que admiten funciones como parámetros
 y devuelven funciones
 """
 def mi_decorador(funcion_a_decorar):
-    def envoltorio():
+    def envoltorio(*args,**kwargs):
         print('------------ Empiezo a Decorar')
-        c = funcion_a_decorar()
+        c = funcion_a_decorar(*args,**kwargs)
+        print(c)
         print('-------------Fin de la decoración')
-
+        
+        return c
     return envoltorio
     
 @mi_decorador
 def saludar():
     print('Hola Mundo!!')
 
-saludar()
+
+def decora_sumar(funcion_que_suma):
+    def identifica(*args):
+        if not type(args) == tuple:
+            lista = tuple(args)
+            return funcion_que_suma(lista)
+        else:
+            return funcion_que_suma(args)
+    return identifica
+    
+
+#saludar()
 
 # resp = mi_decorador(saludar)
 # resp()
-
-# def sumar(a,b):
-#     return a + b
+@decora_sumar
+def sumar(*args):
+    return sum(args)
 
 # def restar(a,b):
 #     return a - b
+
+print(sumar([1,2,3,4,5,6]))
