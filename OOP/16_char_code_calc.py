25c0a7ad53fb88a8e332754da88c91d20314fe06
fernandomolde <ortizmoldef@gmail.com>
2022-01-12 10:31:00 +0100

    OPP

---
+++ OOP/16_char_code_calc.py
@@ -0,0 +1,28 @@
+"""
+Dada una cadena de texto, convertir cada letra a su código ASCII
+con esto creamos un número que llamaremos  total_1.
+Cambiar todos los números '7' por '1' y a ese número le llamaremos total_2
+Devolver total_1 - total_2
+
+"""
+
+"""
+Mono del ejercicio:
+
+cadena de texto es un (STR)
+
+Para convertir cada letra a su codigo ASCII es con ORD o CHR y se llamara total_1
+
+cuando tengamos ya el codigo ASCII cuando haya un numero 7 se cambie por 1 y ese numero se llamara total_2
+
+y por ultimo delvolvemos total_1 - total_2
+
+"""
+
+def code(datos_A):
+    total_1 = []
+    for i in datos_A:
+        total_1.append(ord(i))
+    return code
+
+code(1)
