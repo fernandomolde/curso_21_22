47cedc622b5360f271724bf5acdb7ad0e61f5b0b
fernandomolde <ortizmoldef@gmail.com>
2022-01-18 10:45:49 +0100

    OPP

--- OOP/DNI/EjercicioDni.py
+++ OOP/DNI/EjercicioDni.py
@@ -39,46 +39,63 @@ siguiente tabla para obtener la letra que corresponde:
 """
 
 
-
-from ast import Num
-
-
 class Nif():
-    def __init__(self,Numero=0,letra= '') -> None:
-        self.__num = Numero
-        self.__letra = letra
+    dic_letras = {
+        0: 'T',
+        7: 'F',
+        1: 'R',
+        8: 'P',
+        2: 'W',
+        9: 'D',
+        3: 'A',
+        10:'X',
+        4: 'G',
+        11:'B',
+        5: 'M',
+        12:'N',
+        6: 'Y',
+        13:'J',
+        14:'Z',
+        21:'K',
+        15:'S',
+        22:'E',
+        16:'Q',
+        17:'V',
+        18:'H',
+        19:'L',
+        20:'C',
+    }
+
+    def __init__(self,numero=0,letra= ' '):
+        self.__num = numero
+        if numero:
+            self.__letra = self.__calc_letra()
+        else:
+            self.__letra = ' '
 
 
-    def calc_letra(self,numero):
-        numero = numero % 23 
-        return numero
-
-    
+    def __str__(self) -> str:
+        return f'{self.__num}-{self.__letra}'
 
 
+    def __calc_letra(self):
+        resto = self.__num % 23
+        return self.dic_letras[resto]
 
-
-
+     
 
 #Getter y setters
     @property
-    def __num(self):
-        return self.num
-    
-    @__num.setter
-    def __num(self,numero):
-        self.num = numero
-    
+    def numero(self):
+        return self.__num
+
     @property
-    def __letra(self):
-        return self.letra
+    def letra(self):
+        return self.__letra
     
-    @__letra.setter
-    def __letra(self,letra):
-        self.letra = letra
+    @numero.setter
+    def numero(self, numero):
+            self.__num = numero
+            self.__letra = self.__calc_letra()
     
-
-
-
-p = Nif(12312,'H')
-print(p)
+    
